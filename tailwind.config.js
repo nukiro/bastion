1cc896e4508d77cd972a5e7dbbd7e9552d5cdf00
Carlos Aguilar Miguel <nukiro@yahoo.com>
2026-02-16 23:31:03 +0100

    Add tailwind css to the website

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: ["./website/**/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                abyss:      '#0B101E',
+                navy:       '#16203A',
+                slate:      '#2A3655',
+                cyan:       '#00D4FF',
+                purple:     '#8A2BE2',
+                electric:   '#2D5BFF',
+                gold:       '#00FF9D',
+                drop:       '#FF3366',
+                quarantine: '#FFBE0B',
+                clean:      '#F0F4F8',
+                muted:      '#94A3B8',
+            }
+        }
+    }
+}
